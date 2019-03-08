ef88b117bfd7964414ddd842c94f88fe36418c57
yuezihe <you@example.com>
2019-03-08 17:42:45 +0800

    update

---
+++ src/views/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="register">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="" scoped>
+  
+</style>
