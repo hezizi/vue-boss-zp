b6da3a9d644e0f6b12a9b7c5d97f04946552a136
hezizi <yucihent@sina.com>
2019-03-11 22:41:50 +0800

    使用iview

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { Button, Alert } from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.component('Button', Button);
+Vue.component('Alert', Alert);
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="register">
-    
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="text">Text</Button>
+    <br><br>
+    <Button type="info">Info</Button>
+    <Button type="success">Success</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="error">Error</Button>
+
+
+    <Alert>An info prompt</Alert>
+    <Alert type="success">A success prompt</Alert>
+    <Alert type="warning">A warning prompt</Alert>
+    <Alert type="error">An error prompt</Alert>
   </div>
 </template>
 
