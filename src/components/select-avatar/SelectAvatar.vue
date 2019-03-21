ac78378a08ccc97bacf48c2c822c1d18e90e3a08
yuezihe <you@example.com>
2019-03-21 17:50:58 +0800

    图像选择

--- src/components/select-avatar/SelectAvatar.vue
+++ src/components/select-avatar/SelectAvatar.vue
@@ -6,7 +6,15 @@
       v-model="modal"
       :mask-closable="false"
     >
-      
+      <div class="avatar-wrapper df">
+        <div 
+          class="avatar df-aic df-jcc cp"
+          v-for="item in avatarList" :key="item.id"
+        >
+          <img :src="item.pic" alt="">
+          <p>{{item.text}}</p>
+        </div>
+      </div>
     </Modal>
   </div>
 </template>
@@ -16,13 +24,78 @@ export default {
   name: 'selectavatar',
   data() {
     return {
-      modal: false
+      modal: false,
+      avatarList: [{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },{
+        id: 1,
+        pic: require('../../assets/images/boy.png'),
+        text: 'boy'
+      },{
+        id: 2,
+        pic: require('../../assets/images/girl.png'),
+        text: 'girl'
+      },]
     }
   },
-  
+
 }
 </script>
 
 <style lang="scss" scoped>
-  
+  .avatar-wrapper {
+    .avatar {
+      flex-direction: column;
+      flex-wrap: wrap;
+      padding: 4px;
+      margin: 0 4px;
+      // width: 60px;
+      flex: 1;
+      border: 1px solid transparent;
+      img {
+        margin-bottom: 6px;
+      }
+      &:hover {
+        border: 1px solid #eee;
+      }
+    }
+  }
 </style>
