3326c0f02d032424ff7884bc2da2fb9bc15ee03d
yuezihe <you@example.com>
2019-08-05 17:44:25 +0800

    uypdate

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,7 +3,8 @@
  * 只能处理同步操作，直接与state交互
  */
 
-import { AUTH_SUCCESS, ERR_MSG } from './mutation-type';
+import * as type from './mutation-type';
+// import { AUTH_SUCCESS, ERR_MSG } from './mutation-type';
 import Utils from '@/utils/index';
 
 export default {
@@ -13,7 +14,7 @@ export default {
   // },
   
   // 验证通过将用户信息保存到 state 中
-  [AUTH_SUCCESS] (state, user) {
+  [type.AUTH_SUCCESS] (state, user) {
     localStorage.setItem('userId', user._id)
     state.user = {
       ...user, 
@@ -21,7 +22,7 @@ export default {
     };
   },
   // 验证失败将错误信息保存到 state 中
-  [ERR_MSG] (state, msg) {
+  [type.ERR_MSG] (state, msg) {
     // state.user.msg = msg;
     // Vue.set(state.user, 'msg', msg)
     state.user = {msg}
